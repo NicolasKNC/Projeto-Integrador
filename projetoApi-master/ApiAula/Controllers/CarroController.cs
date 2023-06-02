fccd9ad8e263af55fa7d945f4481ed6925fcc4d1
NicolasKNC <nicolaskcaldeira@gmail.com>
2023-06-02 05:21:33 -0300

    adicionando backend

---
+++ projetoApi-master/ApiAula/Controllers/CarroController.cs
@@ -0,0 +1,68 @@
+﻿using ApiAula.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAula.Controllers
+{
+    [ApiController]
+
+    [Route("teste/Carro")]
+
+    public class CarroController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+
+        //Cadastro
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Carro produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+            return produto.Id;
+        }
+
+        //Leitura todos Elementos da Tabela do Banco
+        [HttpGet]
+        public async Task<ActionResult<List<Carro>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        //Leitura de um Elmento do banco dados pelo ID
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Carro produto;
+            produto = await _context.Produtos.FirstOrDefaultAsync(p => p.Id == id);
+
+            // 如果未找到车辆，则返回找不到的结果
+            if (produto == null)
+                return NotFound("O carro não existe.");
+
+            // 如果找到车辆，则删除它
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return Ok("O carro foi excluído com sucesso.");
+        }
+
+        //Atualizar Conteudo no SGBD
+        [HttpPut]
+        public async Task<ActionResult> Atualizar(Carro produto)
+        {
+            _context.Attach(produto).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ projetoApi-master/ApiAula/Controllers/UtilizadorController.cs
@@ -0,0 +1,62 @@
+﻿using ApiAula.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAula.Controllers
+{
+    [ApiController]
+
+    [Route("api/teste/Utilizador")]
+
+    public class UtilizadorController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UtilizadorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Utilizador Utilizador)
+        {
+            _context.utilizadors.Add(Utilizador);
+            await _context.SaveChangesAsync();
+            return Utilizador.Id;
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Utilizador utilizador;
+            utilizador = await _context.utilizadors.FirstOrDefaultAsync(p => p.Id == id);
+
+            // 如果未找到用户，则返回找不到的结果
+            if (utilizador == null)
+                return NotFound("O utilizador não existe.");
+
+            // 如果找到用户，则删除它
+            _context.utilizadors.Remove(utilizador);
+            await _context.SaveChangesAsync();
+
+            return Ok("O utilizador foi excluído com sucesso.");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Utilizador>>> GetProdutos()
+        {
+            return await _context.utilizadors.ToListAsync();
+        }
+        [HttpPut]
+        public async Task<ActionResult> Atualizar(Utilizador produto)
+        {
+            _context.Entry(produto).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok("O utilizador foi atualizado com sucesso.");
+        }
+    }
+}
